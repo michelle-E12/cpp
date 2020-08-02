d90787f85c1fb117c8c3da5971a80b98219876af
Michelle Oseguera <michelle_espinoza12@outlook.com>
2020-08-01 20:42:53 -0600

    Agregando ejercicios de repaso del 1er parcial

---
+++ Ejercicios/Ejercicio37-Repaso-3/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int b = 10;
+    int c = 4;
+    
+    //if (a > b || a > c)
+    //{
+    //    cout << "a es mayor que b o mayor que c";
+    //}
+
+    //while (a > b)
+    //{
+    //    a--;
+
+    //    if (a == 20)
+    //    {
+    //        break;
+    //    }
+        
+    //}
+
+    while (true)
+    {
+        while (true)
+        {
+            if (b == 10)
+            {
+                break;
+            }
+            b++;
+            
+        }
+        a++;
+        if (a == 1)
+        {
+            break;
+        }
+        
+    }
+    
+    
+    
+
+    return 0;
+}
---
+++ Ejercicios/Ejercicio38-Repaso-4/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+ int main(int argc, char const *argv[])
+{
+    system("cls");
+
+   /* for (int i = 5; i <= 100; i = i + 5)
+    {
+        cout << i << endl;
+
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << j << endl;
+
+            for (int  k = 1; k < 3; k++)
+            {
+                cout << k << endl;
+
+                if(i == 25){
+                    break;
+                }
+
+            }
+
+            if (i == 25)
+            {
+                break;
+            }
+        }
+        if (i == 25)
+        {
+            break;
+        }
+    }
+
+    int i = 5;
+    while (i <= 100)
+    {
+        cout << i << endl;
+        i = i + 5;
+
+        int j;
+        while (j <= 5)
+        {
+            if(i == 25){
+                break;
+            }
+            j++;
+        }
+        
+
+        if (i == 25)
+        {
+            break;
+        }
+    } */
+    
+    /* int i =5;
+    do
+    {
+        cout <<i << endl;
+        i--;
+    } while (i == -1);
+    int j =5;
+    do
+    {
+        cout <<j << endl;
+        j--;
+    } while (j == -1); */
+
+    /* int nota = 70;
+    bool aprobo = nota > 69;
+
+    if (nota > 69){
+        aprobo = true;
+    } else {
+        aprobo = false;
+    }
+
+    while (nota > 69)
+    {
+        cout << " Aprobo! infinitamente";
+    } 
+     cout << aprobo;
+    */
+
+    int a = 5;
+    int b = 10;
+
+    if (a == 5 && b == 7 )
+    {
+        cout << "a o b, es verdad";
+    }
+    
+
+    return 0;
+}
