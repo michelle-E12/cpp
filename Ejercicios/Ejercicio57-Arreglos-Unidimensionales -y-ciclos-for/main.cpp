28a5d247ac58ddacc7844ebf3bb734d7944fe175
Michelle Oseguera <michelle_espinoza12@outlook.com>
2020-08-16 20:51:50 -0600

    Agregando ejercicios de arreglos

---
+++ Ejercicios/Ejercicio57-Arreglos-Unidimensionales -y-ciclos-for/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeros[5]; 
+    // int indice = 50;
+    // numeros[indice] = 3;
+    // cout << numeros[indice] << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        numeros[i] = i;
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout << numeros[i] << endl;
+    }
+    
+    return 0;
+}
